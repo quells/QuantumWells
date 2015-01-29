4933fddc8d4049e4b3e9688125eb8e46bfd830ae
Kai Wells <support@kaiwells.me>
2015-01-28 20:18:51 -0600

    Initial Commit

---
+++ js/constants.js
@@ -0,0 +1,5 @@
+// Universal constants
+var h    = 6.626068 * Math.pow(10, -34);
+var hbar = 1.054571 * Math.pow(10, -34);
+var eV   = 1.602176 * Math.pow(10, -19);
+var c    = 2.997924 * Math.pow(10, 8);
